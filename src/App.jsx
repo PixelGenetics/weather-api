675939d655ce6ce91324d216cfa514589d3e6644
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-01-27 22:14:42 -0600

    Creating logic to change metric system, aldo cleaning code.

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,12 @@ import { useSpring, animated, useTransition } from '@react-spring/web'
 
 function App() {
   const [clima,setClima] = useState([]);
-  const [items,setItems] = useState([]);
-
-  // const [cuadro,setCuadro] = useState([]);
-
-  const transition = useTransition(items,{
-    from:{x: -100, y:800, opacity:0, width:10, height:10},
-    enter: item => async(next) => {
-      await next({ y: item.y, opacity:1, delay: item.delay});
-      await next({ x:0, width:100, height:100});
-    },
-    leave:{x: 100, y:800, opacity:0}
-  });
-
-  // Si no se pone width y height los cuadros no aparecen, aunque el tamaño sea 0, igualmente debe de estar
-  // const transition2 = useTransition(cuadro, {
-  //   from:{x: -1000, y:0, opacity:0, width:10, height:10, color:'#000'},
-  //   enter: cuadro => async(next) => {
-  //     await next({y: cuadro.y, opacity:1, delay: cuadro.delay});
-  //     await next({x:0, width:100, height:100, color:'#fff'});
-  //   },
-  //   leave:{x:1000, y:0, opacity:0}
-  // });
-
+  const [tempUnit,setTempUnit] = useState(0);
+  const [clickedButton, setClickedButton] = useState(false)
   const apikey = '56e1337974ab22108e31121b83bc55f2';
 
-//   useEffect(() => {axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=12.1346&lon=-86.2469&appid=${apikey}`).then(resp => {
-//     console.log('Dentro de useEffect: ',resp.data)
-//     setClima([resp.data.coord])
-// })},[])
 
-const location = () => {
+const location = async () => {
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition((position) => {
       console.log("dentro de if")
@@ -44,6 +19,7 @@ const location = () => {
       axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${nPositionLatitude}&lon=${nPositionLongitude}&appid=${apikey}`).then(resp => {
         console.log("resp: ",resp.data.weather[0].description)
         setClima(resp.data);
+        setTempUnit(resp.data.main.temp);
       })
     }, (error) => {
       alert(error.message)
@@ -53,101 +29,37 @@ const location = () => {
   }
 }
 
-useEffect(() => {
-})
-// const [springs, api] = useSpring(() => ({
-//   from: { x: 0 }
-// }))
+const celsius = () => {
+  setClickedButton(!clickedButton);
+  setTempUnit(clickedButton ? clima.main.temp :  Math.floor(clima.main.temp - 273.15 )+ "°C");
+};
 
-// const handleClick = () => {
-//   api.start({
-//     from: {
-//       x:0.
-//     },
-//     to: {
-//       x:100.
-//     },
-//     leave:{
-//       y:200
-//     }
-//   })
-// }
-
-
-  const cod = () => {
-    return clima.map((obj) => (
-      <animated.div key={obj} >
-        <p>lat: {obj}</p>
-      </animated.div>
-    ))
-  }
+const faren = () => {
+  setClickedButton(!clickedButton);
+  setTempUnit(clickedButton ? clima.main.temp: Math.floor(clima.main.temp-273.15)* 9/5 + 32 + "°F")
+}
 
   return (
     <div className='app'>
-    {/* Cuadros   */}
-    <button onClick={() => {setItems(v => v.length ? [] : [
-      {y: -100, delay:200},
-      {y: -50, delay: 400},
-      {y: 0, delay:600}
-    ]);
-    }}>
-      {items.length ? 'Salir' : 'Entrar'}
-    </button>
-    <div className='container'>
-      {transition((style,item) => item ? <animated.div style={style} className='item'/> : '')}
-    </div>
-
-
     <button onClick={location}>Share location</button>
-    {clima && (
+    {clima && Object.keys(clima).length > 0 &&(
       <div>
         <div>
-          <h2>{clima.name}</h2>
-          <p>{clima && clima.weather ? clima.weather[0].description : 'Presiona el boton para mostrar informacion'}</p>
+          {console.log(clima)}
+          <h2>{clima.name},{clima.sys.country}</h2>
+          <p>{clima.weather[0].description}</p>
+          <p>Wind Speed {clima.wind.speed}</p>
+          <p>Clouds {clima.clouds.all}%</p>
+          <p>Pressure: {clima.main.pressure}Pa</p>
+          <p>Temperature: {tempUnit}</p>
+          <button onClick={faren}>Change to °F</button>
+          <button onClick={celsius}>Change to °C</button>
         </div>
         <div>
         </div>
       </div>
     )}
-    {/* Fin Cuadros  */}
-      {/* {clima.map((item) => (
-        <div key={item}>
-          <p>lat: {item.lat}</p>
-        </div>
-      ))
-      } */}
-      
 
-    {/*  Cuadro  */}
-    {/* Creando logica para cuando clickee el boton que muestre el cuadro o cuadros */}
-    {/*
-    <button onClick={() => {setCuadro(det => det.length ? [] : [
-      {y: -100, delay:100},
-    ]);
-    }}>{cuadro.length ? 'Ocultar' : 'Mostrar'}</button>
-    <div className='container2'>
-      {transition2((style,cuadro) => cuadro ? (
-        <animated.div style={style} className='item2'> {clima[0].lon}</animated.div>
-      ): '')}
-    </div>
-
-  */}
-
-        {/*  Animacion de cuadro */}
-
-
-      {/* <animated.div
-      onClick={handleClick}
-      style={{
-        width: 80,
-        height: 80,
-        background: '#ff6d6d',
-        borderRadius: 8,
-        ...springs
-      }}
-
-      /> */}
-      {/* Fin Cuadro */}
     </div>
 
   )}
